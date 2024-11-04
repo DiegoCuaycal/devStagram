d9e9019fb322e56b98e31893f4d3e80894b56d71
Diego Cuaycal <diegocuaycal01@gmail.com>
2024-11-04 11:23:14 -0500

    Validacion del formulario de login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,4 +11,16 @@ class RegisterController extends Controller
         
     return view('auth.register');
     }
+
+    public function store(Request $request)
+    {
+    // Validación
+    $request->validate([
+        'name' => 'required|max:30',
+        'username' => 'required|unique:users|min:3|max:30',
+        'email'=>'required|unique:users|email|max:60',
+        'password'=>'required' // o aplica otra regla si es necesario
+        ]);
+    }
+
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,9 +10,10 @@
             <img src="{{ asset('img/registrar.jpg') }}" alt="Registro de usuarios">
         </div>
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-2xl">
-            <form action="">
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
                 <div class="mb-5">
-                    <label for="nombre" class="mb-2 block uppercase text-gray-500 font-bold" for="">
+                    <label for="nombre" class="mb-2 block uppercase text-gray-500 font-bold" >
                         Nombre
                     </label>
                     <input 
@@ -20,25 +21,35 @@
                         name="name"
                         type="text"
                         placeholder="Tu nombre"
-                        class="border p-3 w-full rounded-lg">
+                        class="border p-3 w-full rounded-lg @error('name') border-red-500 
+                            
+                        @enderror"
+                        value="{{ old('name')}}"
+                        >
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
 
                 </div>
 
                 <div class="mb-5">
-                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold" for="">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold" >
                         Username
                     </label>
                     <input 
                         id="username"
-                        name="Username"
+                        name="username"
                         type="text"
                         placeholder="Tu nombre de usuario"
                         class="border p-3 w-full rounded-lg">
+                    @error('Username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
 
                 </div>
 
                 <div class="mb-5">
-                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold" for="">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
                         Email
                     </label>
                     <input 
@@ -47,12 +58,17 @@
                         type="text"
                         placeholder="Tu Email de Registro"
                         class="border p-3 w-full rounded-lg">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
+                    
+                    
 
                 </div>
 
                 
                 <div class="mb-5">
-                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold" for="">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold" >
                         Password
                     </label>
                     <input 
@@ -61,6 +77,9 @@
                         type="password"
                         placeholder="Password de registro"
                         class="border p-3 w-full rounded-lg">
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
 
                 </div>
 
